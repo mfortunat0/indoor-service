2316bebc7c7edf7ceb9d30fcf5b429580935d68c
matheus <matheus@workstation.itagua>
2024-12-11 10:23:26 -0300

    update

--- index.ts
+++ index.ts
@@ -1,25 +1,52 @@
-import puppeteer from "puppeteer";
+import liveServer from "live-server";
+import express from "express";
+import cors from "cors";
 import path from "path";
+import fs from "fs";
+import multer from "multer";
 
-(async () => {
-  const browser = await puppeteer.launch({
-    headless: false,
-    timeout: 0,
-    ignoreDefaultArgs: ["--enable-automation"],
-    args: [
-      "--kiosk",
-      "--app",
-      "--disable-infobars",
-      "--silent",
-      "--disable-gpu",
-      "--disable-extensions",
-      "--disable-dev-shm-usage",
-      "--disable-setuid-sandbox",
-      "--start-fullscreen",
-    ],
-    defaultViewport: null,
-    protocolTimeout: 120000,
+const configLiveServer = {
+  port: 5500,
+  file: "index.html",
+};
+
+const ciStorage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    const type = file.mimetype.split("/")[0];
+    if (type === "image") {
+      cb(null, path.join(__dirname, "assets", "ci", "images"));
+    }
+    if (type === "video") {
+      cb(null, path.join(__dirname, "assets", "ci", "videos"));
+    }
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  },
+});
+
+const uploadCi = multer({ storage: ciStorage });
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.post("/ci", uploadCi.single("file"), (req, res) => {
+  res.send();
+});
+
+app.delete("/ci", (req, res) => {
+  const { type, name } = req.body;
+  fs.unlinkSync(path.join(__dirname, "assets", "ci", type, name));
+  res.status(201).send();
+});
+
+app.get("/listFilesCi", (req, res) => {
+  res.json({
+    images: fs.readdirSync(path.join(__dirname, "assets", "ci", "images")),
+    videos: fs.readdirSync(path.join(__dirname, "assets", "ci", "videos")),
   });
-  const page = await browser.newPage();
-  await page.goto(`file://${path.join(__dirname, "index.html")}`);
-})();
+});
+
+liveServer.start(configLiveServer);
+app.listen(5501, () => console.log("File server running"));
