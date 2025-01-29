7765cca3993f8c8828a03ceed64fa48257ac9db2
mfortunat0 <matheus.xmaz10@gmail.com>
2025-01-29 17:21:19 -0300

    update

---
+++ setup.sh
@@ -0,0 +1,18 @@
+apt-get update && \
+apt-get full-uprgade -y && \
+apt-get install ffmpeg git && \
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash && \
+source ~/.bashrc && \
+nvm install 22 && \
+npm i -g yarn pm2 && \
+sudo echo "export LOCAL_IP=$(ip addr show eno1 | grep 'inet ' | cut -d/ -f1 | sed "s/    inet //")" >> ~/.bashrc && \
+git clone https://github.com/mfortunat0/indoor-service && \
+cd indoor-service && \ 
+OLD_IP=192.168.100.134 && \
+sed -i "s|$OLD|$LOCAL_IP|g" index.html && \
+yarn && \
+cd .. && \
+git clone https://github.com/mfortunat0/indoor-client-web && \
+cd indoor-client-web && \
+yarn 
+
