846ce15580b876b8b35547845cfe708392b55ba9
Matheus fortunato <matheus.xmaz10@gmail.com>
2025-01-29 17:55:33 -0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,7 +5,7 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash 
 source ~/.bashrc && \
 nvm install 22 && \
 npm i -g yarn pm2 && \
-sudo echo "export LOCAL_IP=$(ip addr show eno1 | grep 'inet ' | cut -d/ -f1 | sed "s/    inet //")" >> ~/.bashrc && \
+sudo echo "export LOCAL_IP=$(ip addr show enp0s3 | grep 'inet ' | cut -d/ -f1 | sed "s/    inet //")" >> ~/.bashrc && \
 git clone https://github.com/mfortunat0/indoor-service && \
 cd indoor-service && \ 
 OLD_IP=192.168.100.134 && \
